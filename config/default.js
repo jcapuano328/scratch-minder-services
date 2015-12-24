a3ef4a0275513b423297e4a2f9d5d18ffa077833
Jeff Capuano <jcapuano328@gmail.com>
2015-12-24 17:40:02 -0500

    server main entry point

--- config/default.js
+++ config/default.js
@@ -8,9 +8,9 @@ module.exports = {
             levels: ['error', 'warn', 'info', 'debug', 'trace'],
             transports: { // logging transports
                 console: {
-                    format: ['date', 'level', 'message', 'user', 'platform', 'locale', 'stack'],
-                    level: 'info', // maximum level of logged messages
-                    enabled: false, // this switch can be used to easily toggle use of a given transport
+                    format: ['date', 'level', 'message'],
+                    level: 'warn', // maximum level of logged messages
+                    enabled: true, // this switch can be used to easily toggle use of a given transport
                     colorize: true, // this switch can be used to easily toggle use of colors
                     json: false	 // plain text or json output
                 }
